e9b3e253abdca2162acfa98f2e3108eae3803829
Atomi <Atomi@LAPTOP-2PMC52BP>
2021-02-24 20:45:26 -0600

    Primer comentario

---
+++ src/numpares_66123/NumPares_66123.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares_66123;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Atomi
+ */
+public class NumPares_66123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Imprimir los 47 primeros numeros pares
+        
+        Scanner miScanner = new Scanner(System.in);
+         int numero=0;
+           int limite=100;
+             while(numero <= 100){
+               if(numero%2==0){
+    System.out.println(+numero);
+   }
+           numero++;
+ }
+}
+}
+    
+
